3f349817a5af022d5ef2c9061e3b90e3ae979f1a
Michal Tesler <michal.tesler@gmail.com>
2022-11-21 13:45:43 +0100

    Update code comments

--- div_stocks.py
+++ div_stocks.py
@@ -15,7 +15,6 @@ def load_data():
 df = load_data()
 
 # get stock info
-
 tickers = df['Ticker symbol']
 
 # current portfolio
@@ -36,14 +35,12 @@ for ticker in tickers:
     price = info.get('currentPrice')
     capitalization = info.get('marketCap')
     eps = info.get('trailingEps')
-    # eps growth rate?
     if price is None or eps is None:
         price_to_earning_ratio = None
     else:
         price_to_earning_ratio = round(price / eps, 2)
     div = info.get('trailingAnnualDividendRate')
     div_yield = info.get('dividendYield')
-    # div growth rate?
     div_payout_ratio = info.get('payoutRatio')
     if ticker in portfolio_df.values:
         price_paid_for_share = portfolio_df.loc[portfolio_df['ticker']
@@ -55,9 +52,7 @@ for ticker in tickers:
     else:
         yield_on_cost = round(div / price_paid_for_share, 2)
     debt_to_equity = info.get('debtToEquity')
-    # credit rating?
     peg = info.get('pegRatio')
-    # div growth streak?
     number_of_analysis = info.get('numberOfAnalystOpinions')
 
     data = data.append({'ticker': ticker, 'name': name, 'sector': sector, 'outstanding': outstanding, 'price': price,
